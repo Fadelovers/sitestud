f73d8c03991357804a6a58c40ea987f866a62aaa
Fadelovers <normankaety@gmail.com>
2025-05-29 03:42:14 +0300

    h

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ type PageData struct {
 }
 
 func connectToDB() (*sql.DB, error) {
-	dsn := os.Getenv("DATABASE_URL")
+	dsn := os.Getenv("DATABASE_PUBLIC_URL")
 	if dsn == "" {
 		dsn = "host=localhost port=5432 user=postgres password=123 dbname=postgres sslmode=disable"
 	}
